57abb1b091128223a1062722ae0f6d1dae45840d
dhruv <dhruvpt933@gmail.com>
2025-09-03 17:57:38 +0530

    Implemented an 'Complete' and 'Exit' button and functionality

--- gui.py
+++ gui.py
@@ -7,10 +7,15 @@ add_button = sg.Button("Add")
 display_box = sg.Listbox(values=functions.get_todos(), key='todos',
                          enable_events=True, size=[45, 10])
 edit_button = sg.Button('Edit')
+complete_button = sg.Button("Complete")
+exit_button = sg.Button('Exit')
 
 window = sg.Window('My todo app',
-                   layout=[[label], [input_box, add_button], [display_box, edit_button]],
-                   font=('Times New Roman', 12))
+                   layout=[[label], [input_box, add_button],
+                           [display_box, edit_button,complete_button],
+                           [exit_button]],
+                   font=('Helvetica', 13))
+
 while True:
     event, values = window.read()
 
@@ -35,8 +40,21 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case 'Exit':
+            break
+
         case 'todos':
-            window['todos'].update(values=values['todos'])
+            window['todo'].update(value=values['todos'][0] if values['todos'] else '')
 
         case sg.WINDOW_CLOSED:  # Fixed: no parentheses
             break
